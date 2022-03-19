6333a4fe3e57cbf5caafd34877bb04a138785b71
simon Nkotanyi <simon1nkotanyi@gmail.com>
2022-03-19 22:20:32 +0000

    my firdt betty commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,10 @@
  *Return: return 0, always (success)
  */
 int main(void)
-{	printf("Size of a char: %ld bytes\n", sizeof(long unsigned int));
-	printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
-	printf("Size of a float: %ld bytes\n", sizeof(long unsigned int));
-	printf("Size of an int: %ld bytes\n", sizeof(long unsigned int));
-	printf("Size of a long int: %ld bytes\n", sizeof(long unsigned int));
+{	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
