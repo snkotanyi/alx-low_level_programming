62d3c04ab0221b27c384702de0e7f4a9f5d8287f
simon Nkotanyi <simon1nkotanyi@gmail.com>
2022-03-19 21:43:44 +0000

    my betty commits

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print size of various data types
+ *
+ *Return: return 0, always (success)
+ */
+int main(void)
+{	printf("Size of a char: %ld bytes\n", sizeof(long unsigned int));
+	printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes\n", sizeof(long unsigned int));
+	printf("Size of an int: %ld bytes\n", sizeof(long unsigned int));
+	printf("Size of a long int: %ld bytes\n", sizeof(long unsigned int));
+	return (0);
+}
+
