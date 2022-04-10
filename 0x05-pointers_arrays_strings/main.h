065e2fce9b12bb9daeba06cfe9c1fd4a9c32d047
simon Nkotanyi <simon1nkotanyi@gmail.com>
2022-04-10 13:18:42 +0000

    My first pointer protype

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef PROTOPTYPES
+#define PROTOTYPES
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+
+#endif
+
